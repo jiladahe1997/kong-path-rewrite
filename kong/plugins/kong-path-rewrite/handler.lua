bd71ae35f355e413eb9e25f1685780f611e12f25
Ren Mingrui <972931182@qq.com>
2019-12-18 22:39:05 +0800

    1.增加日志

--- kong/plugins/kong-path-rewrite/handler.lua
+++ kong/plugins/kong-path-rewrite/handler.lua
@@ -13,6 +13,7 @@ function reWriteUrlHandle:access(config)
   reWriteUrlHandle.super.init_worker(self)
   local originPath = kong.router.get_service().path
   kong.service.request.set_path(originPath..config.rewritePath)
+  kong.log("originPath",originPath, "rewritePath",config.rewritePath)
 end
 
 return reWriteUrlHandle
